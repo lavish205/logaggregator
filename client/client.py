b92d1fbff426c51c727916f353ca247077681db2
Lavish Aggarwal <lucky.lavish@gmail.com>
2016-04-29 02:25:57 +0530

    code optimized

--- client/client.py
+++ client/client.py
@@ -22,58 +22,35 @@ def worker1():
         print "worker running"
         urls = ['http://localhost:8888/test/']
         while True:
-            # print tasks
             print "getting task from queue worker 1"
             task = tasks.get()
-            # print task
-            rs = requests.post(urls[0], task)
-            if not rs.status_code == 200:
-                tasks.put(task)
-            # rs = (grequests.post(u, data=task) for u in urls)
-            # req = gevent.spawn(grequests.map, rs)
-            # gevent.joinall([req])
-            print task
+            
+            rs = (grequests.post(u, data=task) for u in urls)
+            grequests.map(rs)
+
     except Empty:
         print "empty"
         pass
     except ConnectionError:
         tasks.put(task)
+
 
 def worker2():
     try:
         print "worker running"
         urls = ['http://localhost:8888/test/']
         while True:
-            # print tasks
             print "getting task from queue worker 2"
             task = tasks.get()
-            # print task
-            rs = requests.post(urls[0], task)
-            if not rs.status_code == 200:
-                tasks.put(task)
-            # rs = (grequests.post(u, data=task) for u in urls)
-            # req = gevent.spawn(grequests.map, rs)
-            # gevent.joinall([req])
-            print task
+
+            rs = (grequests.post(u, data=task) for u in urls)
+            grequests.map(rs)
+
     except Empty:
         print "empty"
         pass
     except ConnectionError:
         tasks.put(task)
-
-
-def send_data(data, service, client, ip):
-    print "sending"
-    urls = ['http://localhost:8888/test/']
-    params = {
-        'data': data,
-        'service': service,
-        'client': client,
-        'client_ip': ip
-    }
-    rs = (grequests.post(u, data=params) for u in urls)
-    req = gevent.spawn(grequests.map, rs)
-    gevent.joinall([req])
 
 
 def get_logs(service, client, client_ip):
@@ -87,7 +64,6 @@ def get_logs(service, client, client_ip):
     except IOError as e:
         logging.error(e)
 
-    # Find the size of the file and move to the end
     st_results = os.stat(file_path)
     st_size = st_results[6]
     f_obj.seek(st_size)
@@ -96,7 +72,7 @@ def get_logs(service, client, client_ip):
         where = f_obj.tell()
         line = f_obj.readline()
         if not line:
-            time.sleep(1)
+            gevent.sleep(1)
             f_obj.seek(where)
         else:
             t = {
@@ -105,10 +81,8 @@ def get_logs(service, client, client_ip):
                 'client': client,
                 'client_ip': client_ip
             }
-            print "adding task to queuw"
+            print "adding task to queue"
             tasks.put(t)
-            # r = gevent.spawn(send_data, line, service_name, client, client_ip)
-            # gevent.joinall([r])
 
 
 def main():
@@ -116,12 +90,10 @@ def main():
         config = json.load(open('config.json', 'r'))
         client = config.get('client_name')
         client_ip = config.get('client_ip')
-        # print "got config file"
 
         req = []
         for service in config.get('services', []):
             req.append(gevent.spawn(get_logs, service, client, client_ip))
-
         gevent.joinall(req)
 
     except IOError as e:
